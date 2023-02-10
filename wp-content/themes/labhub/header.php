66ca1d0eb64175178e9348777099b992a1cf58ac
ElyesMaamouri <88774647+ElyesMaamouri@users.noreply.github.com>
2023-02-10 17:03:19 +0100

    Add footer and header

---
+++ wp-content/themes/labhub/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charser') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+
+        <header>
+            <section class="top-bar">
+                <div class="logo">Logo</div>
+                <div class="searchbox">
+                    search
+
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
---
+++ wp-content/themes/labhub/index.php
@@ -0,0 +1,17 @@
+
+<?php get_header();?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <section class="hero">
+            hero
+        </section>
+        <section class="services">
+            Services
+        </section>
+        <section class="home-blog">
+
+            Blog
+        </section>
+    </main>
+</div>
+<?php  get_footer(); ?>
